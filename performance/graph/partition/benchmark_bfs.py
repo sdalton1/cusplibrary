9cf9c12efe565f5b50c90c7cd7b66e9997ea5f84
Steve <sdalton1@gmail.com>
2016-01-29 03:05:57 -0800

    Updated BFS benchmark

--- performance/graph/partition/benchmark_bfs.py
+++ performance/graph/partition/benchmark_bfs.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from pylab import savefig
-from scipy.io import mmread
+from scipy.io import mmread, mmwrite
 
 from matplotlib import rc
 rc('font',**{'family':'serif','serif':['Computer Modern']})
@@ -40,21 +40,50 @@ unstructured_mats = [ mat + (path,) for mat in unstructured_mats]
 # assemble suite of matrices
 trials = unstructured_mats
 
+def crop_figure(fullname):
+    os.system('pdfcrop ' + fullname + ' ' + fullname)
+
+def save_figure(plt, name, ext='.pdf'):
+    fullname = name + ext
+    plt.savefig('./' + fullname)
+    crop_figure(fullname)
+
 def run_tests(num_iterations):
 
     # run benchmark for each file
     for matrix,filename,path in trials:
         matrix_filename = path + filename
-        A = mmread(matrix_filename);
+        A = mmread(matrix_filename)
+
+        bfs_matrix = np.zeros((num_iterations,2))
 
         # execute the benchmark on this file
-        for source in np.random.randint(0, A.shape[0], size=num_iterations) :
+        for index,source in enumerate(np.random.randint(0, A.shape[0], size=num_iterations)) :
             # setup the command to execute
             # proc = subprocess.Popen([binary_filename, matrix_filename, str(source)], stdout=subprocess.PIPE, shell=True)
             proc = subprocess.Popen([binary_filename, matrix_filename, str(source)], stdout=subprocess.PIPE)
             (out, err) = proc.communicate()
-            print out
-            # bfs_data = out.split("\n")[2].split(",")
-            # print bfs_data
+            bfs_data = out.split("\n")[2].split(",")
+            bfs_time = bfs_data[0].split(":")[1].split()[0]
+            bfs_levels = bfs_data[1].split(":")[1].split()[0]
 
-run_tests(1)
+            bfs_matrix[index,0] = bfs_time
+            bfs_matrix[index,1] = bfs_levels
+
+        # outfile_name = matrix + "_bfs.mtx"
+        # mmwrite(outfile_name, bfs_matrix)
+        # print "Wrote data file: {}".format(outfile_name)
+
+        plt.figure()
+        ax = plt.subplot(111)
+        ax.plot(bfs_matrix[:,1], bfs_matrix[:,0], 'o')
+        ax.set_xlabel("Number of Levels")
+        ax.set_ylabel("Time (ms)")
+        ax.set_title(matrix.replace("_","\\_"))
+        # save_figure(plt, "bfs_levels_perf_{}".format(matrix))
+
+    plt.show()
+
+if __name__=="__main__":
+
+    run_tests(10)
